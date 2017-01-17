2cf73431bbe9165d96fe87e3a616eda85819fa57
3ijtKwijt <3ijtKwijt@gmail.com>
2017-01-17 22:19:39 +0100

    Create RainbowRole.js

---
+++ RainbowRole.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({autoReconnect:true});
+const config = require('./config.json');
+
+var size    = config.colors;
+var rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = "#"+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? "0"+hex : hex;
+}
+var place = 0;
+var changeColor = function() {
+	client.guilds.get(config.serverId).roles.find('name', config.roleName).setColor(rainbow[place])
+	.catch(console.error);
+	
+	if(config.logging){
+		console.log("[ColorChanger] Changed color to " + rainbow[place]);
+	}
+	if(place == (size - 1)){
+		place = 0;
+	}else{
+		place++;
+	}
+}
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.username}!`);
+	setInterval(changeColor, config.speed);
+});
+
+
+client.login(config.token);
