bc9c7bfe58955a0cc05a93f98ef68f63f6e22099
ItsJordan <ItsJordan@users.noreply.github.com>
2017-01-17 18:48:08 -0500

    Update RainbowRole.js
    1) Auto reconnect is enabled by default in v9+ iirc..
    2) SMH using `var` in 2017? 
    3) Seriously.. using a variable to store a function ytho...

--- RainbowRole.js
+++ RainbowRole.js
@@ -1,16 +1,16 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({autoReconnect:true});
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const config = require('./config.json');
 
-var size    = config.colors;
-var rainbow = new Array(size);
+const size    = config.colors;
+const rainbow = new Array(size);
 
 for (var i=0; i<size; i++) {
   var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
   var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
   var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
 
-  rainbow[i] = "#"+ red + green + blue;
+  rainbow[i] = '#'+ red + green + blue;
 }
 
 function sin_to_hex(i, phase) {
@@ -18,31 +18,31 @@ function sin_to_hex(i, phase) {
   var int = Math.floor(sin * 127) + 128;
   var hex = int.toString(16);
 
-  return hex.length === 1 ? "0"+hex : hex;
+  return hex.length === 1 ? '0'+hex : hex;
 }
 
-var place = 0;
-var servers = config.servers;
+let place = 0;
+const servers = config.servers;
 
-var changeColor = function() {
-	for (let index = 0; index < servers.length; ++index) {		
-		client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {		
+    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
 		.catch(console.error);
 		
-		if(config.logging){
-			console.log("[ColorChanger] Changed color to " + rainbow[place] + " in server: " + servers[index]);
-		}
-		if(place == (size - 1)){
-			place = 0;
-		}else{
-			place++;
-		}
-	}
+    if(config.logging){
+      console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+    }
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
 }
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.username}!`);
-	setInterval(changeColor, config.speed);
+  console.log(`Logged in as ${client.user.username}!`);
+  setInterval(changeColor, config.speed);
 });
 
 
