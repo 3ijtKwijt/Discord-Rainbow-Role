9a0194703a1b89eea7b055da132fda464677556d
3ijtKwijt <3ijtKwijt@gmail.com>
2017-01-18 14:42:08 +0100

    Update RainbowRole.js

--- RainbowRole.js
+++ RainbowRole.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({autoReconnect:true});
+const client = new Discord.Client();
 const config = require('./config.json');
 
 var size    = config.colors;
