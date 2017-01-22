064a0cf813088cae506db9508d6616dc9569fb06
3ijtKwijt <3ijtKwijt@gmail.com>
2017-01-22 13:27:47 +0100

    Update RainbowRole.js

--- RainbowRole.js
+++ RainbowRole.js
@@ -42,6 +42,7 @@ function changeColor() {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
+  if(config.speed < 60000){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
   setInterval(changeColor, config.speed);
 });
 
